70129f70ed9de26e5fd6292295e6fc8b2ae463ae
felixgrant <72441151+felixgrant@users.noreply.github.com>
2021-01-26 18:55:48 +1100

    Already8 Initial Commit

---
+++ already8.py
@@ -0,0 +1,18 @@
+# Already8 is a simple program that allows you to select n number of dishes from your cookbook
+# and retrieve a shopping list with the ingredients required to make those dishes.
+
+# import libraries
+
+import pandas as pd
+from datetime import datetime, timedelta
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+import csv
+import random
+import operator
+
+print('Hello sexy')
+
+q = pd.DataFrame()
+print(q)
